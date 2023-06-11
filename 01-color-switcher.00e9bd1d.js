86e1506b2b00b12ff11581039754d33329f1ab51
JorG1990 <110580120+JorG1990@users.noreply.github.com>
2023-06-11 02:47:41 +0000

    Deploying to gh-pages from @ JorG1990/goit-js-hw-09@efaabe00683fdd398a6d67d3ca4bfb611fc5d861 🚀

---
+++ 01-color-switcher.00e9bd1d.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",(function(){var e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),d=document.querySelector("body");t.disabled=!0,e.addEventListener("click",(function(){e.disabled=!0,t.disabled=!1,intervalId=setInterval((function(){d.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),t.addEventListener("click",(function(){return e.disabled=!1,t.disabled=!0,clearInterval(intervalId)}))}));
+//# sourceMappingURL=01-color-switcher.00e9bd1d.js.map
