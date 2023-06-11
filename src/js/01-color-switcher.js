f938ec42c6e7b8eaa8a92271704dbdb462c27549
JorG1990 <jorge  garcia>
2023-06-10 22:01:25 -0500

    js-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,8 +2,6 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }
-
-
   const startButton = document.querySelector("[data-start]");
   const stopButton = document.querySelector("[data-stop]");
   const body = document.querySelector("body");
@@ -26,27 +24,3 @@ function getRandomHexColor() {
   startButton.addEventListener("click", startColorSwitch);
   stopButton.addEventListener("click", stopColorSwitch);
 
-
-// const body = document.querySelector("body");
-// const btnStart = document.querySelector("[data-start]");
-// const btnStop = document.querySelector("[data-stop]");
-
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
-// }
-
-// let timerId = null;
-// btnStop.disabled = true;
-// btnStart.addEventListener("click", () => {
-//   timerId = setInterval(() => {
-//     body.style.backgroundColor = getRandomHexColor();
-//     btnStart.disabled = true;
-//     btnStop.disabled = false;
-//   }, 1000);
-// })
-
-// btnStop.addEventListener("click", () => {
-//   clearInterval(timerId);
-//   btnStop.disabled = true;
-//   btnStart.disabled = false;
-// });
